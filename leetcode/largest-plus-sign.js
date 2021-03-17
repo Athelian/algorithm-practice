4ed7490ef5d1abed90dd422616a3e9c6501b7302
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2021-03-17 16:15:56 +0900

    attempt find centermost unblocked square, then centermost blocked square

--- leetcode/largest-plus-sign.js
+++ leetcode/largest-plus-sign.js
@@ -5,25 +5,28 @@
  */
 var orderOfLargestPlusSign = function (N, mines) {
   const even = Boolean(N % 2 === 0);
-  let length = even ? 2 : 1;
   const minies = {};
   const minesX = {};
   const minesY = {};
-
+  const queue = [];
+  let length = even ? 2 : 1;
+  let x = even ? N / 2 - 1 : Math.floor(N / 2);
+  let y = x;
+  let max = x + 1;
+  const grid = Array(N)
+    .fill()
+    .map(() => Array(N).fill(1));
   mines.forEach((mine) => {
     if (!minies[mine[0]]) minies[mine[0]] = {};
     minies[mine[0]][mine[1]] = true;
     minesY[mine[0]] = true;
     minesX[mine[1]] = true;
+    grid[mine[0]][mine[1]]--;
+    // grid[mine[0]].fill(0);
+    // for (let i = 0; i < N; i++) {
+    //   grid[i][mine[1]] = 0;
+    // }
   });
-
-  let x = even ? N / 2 - 1 : Math.floor(N / 2);
-  let y = x;
-
-  let best = 0;
-  let max = x + 1;
-
-  const obstruction = (y, x) => (minies[y][x] ? true : false);
 
   const tunnel = (y, x) => {
     let originalX = x;
@@ -77,36 +80,50 @@ var orderOfLargestPlusSign = function (N, mines) {
     return smallest;
   };
 
-  let value;
-  while (best < max) {
+  const spiral = () => {
     for (let i = 0; i < length; i++) {
-      if (!minesX[x] && !mines[y + i]) return max;
+      queue.push([y + i, x]);
     }
     for (let i = 1; i < length; i++) {
-      if (!minesX[x + i] && !mines[y]) return max;
+      queue.push([y, x + i]);
     }
     for (let i = 1; i < length; i++) {
-      if (!minesX[x + length - 1] && !mines[y + i]) return max;
+      queue.push([y + i, x + length - 1]);
     }
     for (let i = 1; i < length - 1; i++) {
-      if (!minesX[x + i] && !mines[y + length - 1]) return max;
+      queue.push([y + length - 1, x + i]);
     }
+  };
 
-    for (let i = 0; i < length; i++) {
-      value = tunnel(y + i, x);
-      if (value > best) best = value;
+  while (x >= 0) {
+    spiral();
+    let i = queue.length - 1;
+    while (i >= 0) {
+      if (grid[queue[i][0]][queue[i][1]] === 1) return max;
+      queue.pop();
+      i--;
     }
-    for (let i = 1; i < length; i++) {
-      value = tunnel(y, x + i);
+    length += 2; //Square extends by two
+    x--; // Move one square up and to the left
+    y--;
+    max--;
+  }
+
+  let value;
+  let best = 0;
+  length = even ? 2 : 1;
+  x = even ? N / 2 - 1 : Math.floor(N / 2);
+  y = x;
+  max = x + 1;
+
+  while (best < max) {
+    spiral();
+    let i = queue.length - 1;
+    while (i >= 0) {
+      value = tunnel(queue[i][0], queue[i][1]);
       if (value > best) best = value;
-    }
-    for (let i = 1; i < length; i++) {
-      value = tunnel(y + i, x + length - 1);
-      if (value > best) best = value;
-    }
-    for (let i = 1; i < length - 1; i++) {
-      value = tunnel(y + length - 1, x + i);
-      if (value > best) best = value;
+      queue.pop();
+      i--;
     }
     length += 2; //Square extends by two
     x--; // Move one square up and to the left
@@ -117,9 +134,11 @@ var orderOfLargestPlusSign = function (N, mines) {
   return best;
 };
 
-orderOfLargestPlusSign(6, [
-  [4, 2],
-  [1, 2],
-  [0, 3],
+orderOfLargestPlusSign(5, [
+  [1, 0],
+  [1, 4],
   [2, 4],
+  [3, 2],
+  [4, 0],
+  [4, 3],
 ]);
