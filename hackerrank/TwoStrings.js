c10524c0168d0eb38cf165bfa0cefd9d29f87993
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2022-01-04 23:07:16 +0900

    Create TwoStrings.js

---
+++ hackerrank/TwoStrings.js
@@ -0,0 +1,10 @@
+function twoStrings(s1, s2) {
+  const map = {}
+  for (let i = 0; i < s1.length; i++) {
+    map[s1[i]] = true
+  }
+  for (let i = 0; i < s2.length; i++) {
+    if (map[s2[i]]) return "YES"
+  }
+  return "NO"
+}
