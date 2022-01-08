40873204cba82d0351858a140f3e0f559202b728
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2022-01-08 22:55:01 +0900

    Update HourglassSum.js

--- hackerrank/HourglassSum.js
+++ hackerrank/HourglassSum.js
@@ -1,10 +1,28 @@
-  while (x < 6) {
-    const topRow = nextTop ? nextTop + arr[y][x + 1] : arr[y][x] + arr[y][x + 1] + arr[y][x + 2]
-    const bottomRow = nextBottom ? nextBottom + arr[y][x + 1] : arr[y + 2][x] + arr[y + 2][x + 1] + arr[y + 2][x + 2]
-    const middle = arr[y + 1][x + 1]
-    const total = topRow + bottomRow + middle
-    if (total > max) max = total
-    nextTop = topRow - arr[y][x + 2]
-    nextBottom = bottomRow - arr[y + 2][x + 2]
-    x++
-  }
+function hourglassSum(arr) {
+  // Write your code here
+  const summedGrid = arr.map((row, i) =>
+    row.map((el, j) =>
+      j === 0 || j === arr.length - 1 ? 0 : row[j - 1] + row[j + 1] + el
+    )
+  );
+  let max = Number.MIN_SAFE_INTEGER;
+
+  arr.forEach((row, i) =>
+    row.forEach((col, j) => {
+      if (i !== 0 && j !== 0 && i !== arr.length - 1 && j !== arr.length - 1) {
+        const total = summedGrid[i - 1][j] + summedGrid[i + 1][j] + arr[i][j];
+        if (total > max) max = total;
+      }
+    })
+  );
+  return max;
+}
+
+hourglassSum([
+  [-1, -1, 0, -9, -2, -2],
+  [-2, -1, -6, -8, -2, -5],
+  [-1, -1, -1, -2, -3, -4],
+  [-1, -9, -2, -4, -4, -5],
+  [-7, -3, -3, -2, -9, -9],
+  [-1, -3, -1, -2, -4, -5]
+]);
